c1a769a979b922bbf3b01867eb23c74c90002307
Kevin Coakley <kcoakley@sdsc.edu>
2017-06-07 08:55:51 -0700

    Removed unused files

--- spark_notebook/remote/init_sc.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-master_url = requests.get("http://localhost:8080/json").json()['url']
--- spark_notebook/remote/spark-ec2/ipython_notebook_config.py
+++
@@ -1,7 +0,0 @@
-from os.path import expanduser
-
-c = get_config()
-c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.notebook_dir = expanduser('~/workspace')
-# c.NotebookApp.password = u'<your_password>'
